d2abffd7e7b4e89e8009465a480f9ab6ee7a8722
your name <hoanghoanghoang2305@gmail.com>
2026-01-24 21:01:35 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,48 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
 
 Route::get('/', function () {
     return view('Home');
 });
-Route::prefix('products')->group(function () {
-    Route::get('/', function () {
-        return view('products.index');
-    });
-    Route::get('/add', function () {
-        return view('products.add');
-    });
-    Route::get('/{id?}', function ($id = '123') {
-        return view('products.show', ['id' => $id]);
-    });
-});
+
+//login
+Route::get('/login', [ProductController::class, 'login']);
+Route::post('/login', [ProductController::class, 'checkLogin']);
+
+
+//Products (Controller)
+
+// Danh sách
+Route::get('/products', [ProductController::class, 'index'])
+    ->name('products.index');
+
+// Form thêm
+Route::get('/products/create', [ProductController::class, 'create'])
+    ->name('products.add');
+
+// Xử lý thêm
+Route::post('/products', [ProductController::class, 'store'])
+    ->name('products.store');
+
+// Chi tiết
+Route::get('/products/{id}', [ProductController::class, 'getDetail'])
+    ->name('products.detail');
+
+
+//sinvien
 Route::get('/sinhvien/{name?}/{mssv?}', function ($name = 'Tran Huy Hoang', $mssv = '0031267') {
-    return view('sinhvien', ['name' => $name, 'mssv' => $mssv]);
+    return view('sinhvien', compact('name', 'mssv'));
 });
+
+//co
 Route::get('/banco/{n}', function ($n) {
-    return view('banco', ['n' => $n]);
+    return view('banco', compact('n'));
 });
+
+//404
 Route::fallback(function () {
     return view('error.404');
 });
